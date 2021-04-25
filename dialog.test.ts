eaacf47d29d17fb503d0b05960e6f626fd81d9aa
Johan Dahl <johan.dalabacka@gmail.com>
2021-04-25 21:31:30 +0200

    dialog
    Fix types and return stderr from showDialog

--- dialog.test.ts
+++ dialog.test.ts
@@ -5,7 +5,8 @@ Deno.test({
   name: "dialog",
   fn: async () => {
     const iconPath = '/Applications/Utilities/Unison LU.app/Contents/Resources/Unison.icns'
-    const result = await showDialog('Test', 'Some test text', ['OK'], iconPath)
+    const result = await showDialog('Test', 'Some test text',
+      { buttons: ['OK'], iconPath })
     console.log({result})
     assertEquals("world", "world");
   },
--- dialog.ts
+++ dialog.ts
@@ -1,35 +1,37 @@
 import { runInShell } from './run.ts'
 
 interface DialogOptions {
-  buttons : string[],
-  iconPath : string,
-  runInBackground : boolean
+  buttons? : string[],
+  iconPath? : string,
+  runInBackground? : boolean
 }
 
 export function showDialog(title : string, description : string,
-  options : DialogOptions = {buttons: ['OK'], iconPath: '', runInBackground: false}) {
+  {buttons, iconPath, runInBackground} : DialogOptions = {buttons: ['OK'], iconPath: '', runInBackground: false}) {
     let cmd = [
       '/Library/Application Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper',
       '-windowType', 'utility',
       '-title', title,
       '-description', description
     ]
-    if (options.buttons.length === 0) {
+    if (! buttons || buttons.length === 0) {
       cmd = cmd.concat(['-button1', 'OK', '-defaultButton', '1'])
-    } else if (options.buttons.length >= 1) {
-      cmd = cmd.concat(['-button1', options.buttons[0], '-defaultButton', '1'])
-    }
-    if (options.buttons.length >= 2) {
-      cmd = cmd.concat(['-button2', options.buttons[1]])
+    } else {
+      if (buttons.length >= 1) {
+        cmd = cmd.concat(['-button1', buttons[0], '-defaultButton', '1'])
+      }
+      if (buttons && buttons.length >= 2) {
+        cmd = cmd.concat(['-button2', buttons[1]])
+      }
     }
     
-    if (options.iconPath) {
-      cmd = cmd.concat('-icon', options.iconPath, '-iconSize', '64')
+    if (iconPath) {
+      cmd = cmd.concat('-icon', iconPath, '-iconSize', '64')
     }
 
-    if (options.runInBackground) {
+    if (runInBackground) {
       cmd.push('&') // To run in background
     }
 
-    return runInShell(cmd, {stderr: undefined})
+    return runInShell(cmd, {stdout: undefined})
 }
